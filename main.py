6881a826998b4419e39992d1c9edf284831c3fa5
shota <syouta74_vision_book@icloud.com>
2019-08-08 22:27:39 +0900

    g

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ import os
 app = Flask(__name__)
 
 
-#環境変数からLINE Access Tokenを設定
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["k5qSFVxKFT6qE34E9Osu9P/9ZwLZO/htMmdUOz3BQuAZVYI7I7fmH2S5ZGKUmzyyEpyWqEyeTww7LLCfjIaxCY66u5xM57OXMMhzvB6JF/m/r48DMLlpX8aDtPXOie5fRw20y+KEMqWssKNv1ngtiQdB04t89/1O/w1cDnyilFU="]
-#環境変数からLINE Channel Secretを設定
-LINE_CHANNEL_SECRET = os.environ["d7cb389c9d0889a691a6f404ee4229f4"]
+#LINE Access Token
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+#LINE Channel Secret
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
-line_bot_api = LineBotApi('k5qSFVxKFT6qE34E9Osu9P/9ZwLZO/htMmdUOz3BQuAZVYI7I7fmH2S5ZGKUmzyyEpyWqEyeTww7LLCfjIaxCY66u5xM57OXMMhzvB6JF/m/r48DMLlpX8aDtPXOie5fRw20y+KEMqWssKNv1ngtiQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('d7cb389c9d0889a691a6f404ee4229f4')
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
 
 @app.route("/")
 def hello_world():
