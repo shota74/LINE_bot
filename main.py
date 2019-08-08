e89b591ca7e32e5d9713ea0ed9a36b98530a38a5
shota <syouta74_vision_book@icloud.com>
2019-08-08 22:11:07 +0900

    hoge

--- main.py
+++ main.py
@@ -42,7 +42,9 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        #TextSendMessage(text=event.message.text))
+        TextSendMessage(text="hoge"))
+
 
 
 if __name__ == "__main__":
