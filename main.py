69ba411e1f0c648eb6b0e3d09907f06a9bc1d5a9
shota <syouta74_vision_book@icloud.com>
2019-08-14 10:29:18 +0900

    aaa

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_SECRET', None)
+LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
