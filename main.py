e70e62cabf04e921c74c6809bcdd8ce72f8469da
shota <syouta74_vision_book@icloud.com>
2019-08-14 09:00:34 +0900

    bb

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import urllib.request
 import os
-import sys
-import json
-import scrape as sc
-from argparse import ArgumentParser
 
 from flask import Flask, request, abort
 from linebot import (
@@ -13,22 +8,15 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+    MessageEvent, TextMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn)
 
 app = Flask(__name__)
 
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', d7cb389c9d0889a691a6f404ee4229f4)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', k5qSFVxKFT6qE34E9Osu9P/9ZwLZO/htMmdUOz3BQuAZVYI7I7fmH2S5ZGKUmzyyEpyWqEyeTww7LLCfjIaxCY66u5xM57OXMMhzvB6JF/m/r48DMLlpX8aDtPXOie5fRw20y+KEMqWssKNv1ngtiQdB04t89/1O/w1cDnyilFU=)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
-line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 
 @app.route("/callback", methods=['POST'])
@@ -38,7 +26,6 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -46,17 +33,35 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+def response_message(event):
+    # notesのCarouselColumnの各値は、変更してもらって結構です。
+    notes = [CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle02.jpg",
+                            title="【ReleaseNote】トークルームを実装しました。",
+                            text="creation(創作中・考え中の何かしらのモノ・コト)に関して、意見を聞けるようにトークルーム機能を追加しました。",
+                            actions=[{"type": "message","label": "サイトURL","text": "https://renttle.jp/notes/kota/7"}]),
 
-    word = event.message.text
-    result = sc.getNews(word)
+             CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle03.jpg",
+                            title="ReleaseNote】創作中の活動を報告する機能を追加しました。",
+                            text="創作中や考え中の時点の活動を共有できる機能を追加しました。",
+                            actions=[
+                                {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/6"}]),
 
-    line_bot_api.reply_message(
-    event.reply_token,
-    TextSendMessage(text=result)
+             CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle04.jpg",
+                            title="【ReleaseNote】タグ機能を追加しました。",
+                            text="「イベントを作成」「記事を投稿」「本を登録」にタグ機能を追加しました。",
+                            actions=[
+                                {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/5"}])]
+
+    messages = TemplateSendMessage(
+        alt_text='template',
+        template=CarouselTemplate(columns=notes),
     )
 
+    line_bot_api.reply_message(event.reply_token, messages=messages)
+
+
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
